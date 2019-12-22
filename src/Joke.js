07eb7843d65cf6893080215d80e911d3d92403a4
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-12-23 03:38:36 +0600

    joke component and stylings

---
+++ src/Joke.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import './Joke.css';
+
+export default class Joke extends Component {
+   getModification() {
+      if (this.props.votes >= 15) {
+         return { color: "#4caf50", emoji: "em em-rolling_on_the_floor_laughing" };
+      } else if (this.props.votes >= 12) {
+         return { color: "#8bc34a", emoji: "em em-laughing" };
+      } else if (this.props.votes >= 9) {
+         return { color: "#cddc39", emoji: "em em-smiley" };
+      } else if (this.props.votes >= 6) {
+         return { color: "#ffeb3b", emoji: "em em-slightly_smiling_face" };
+      } else if (this.props.votes >= 3) {
+         return { color: "#ffc107", emoji: "em em-neutral_face" };
+      } else if (this.props.votes >= 0) {
+         return { color: "#ff9800", emoji: "em em-confused" };
+      } else {
+         return { color: "#f44336", emoji: "em em-angry" };
+      }
+   }
+
+   render() {
+      return (
+         <div className="Joke">
+            <div className="Joke-btns">
+               <i className="fas fa-arrow-up Joke-btns-up" onClick={this.props.upvote} />
+               <span className="Joke-votes" style={{ borderColor: this.getModification().color }}>{this.props.votes}</span>
+               <i className="fas fa-arrow-down Joke-btns-down" onClick={this.props.downvote} />
+            </div>
+
+            <div className="Joke-text">
+               {this.props.text}
+            </div>
+
+            <div className="Joke-emoji">
+               <i className={this.getModification().emoji} aria-role="presentation" aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
+            </div>
+         </div>
+      );
+   };
+};
--- src/JokesList.js
+++ src/JokesList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import uuid from 'uuid/v4';
 import './JokesList.css';
+import Joke from './Joke';
 
 export default class JokesList extends Component {
    static defaultProps = {
@@ -16,21 +18,35 @@ export default class JokesList extends Component {
       let jokes = [];
       while (jokes.length < this.props.numJokesToGet) {
          let response = await axios.get("https://icanhazdadjoke.com/", { headers: { Accept: "application/json" } });
-         jokes.push(response.data.joke);
+         jokes.push({ id: uuid(), text: response.data.joke, votes: 0 });
       }
       this.setState({ jokes: jokes });
    }
 
+   handleVotes(id, delta) {
+      this.setState(st => ({
+         jokes: st.jokes.map(j => (
+            j.id === id ? { ...j, votes: j.votes + delta } : j
+         ))
+      }))
+   }
+
    render() {
       let joke = this.state.jokes.map(j => (
-         <div>{j}</div>
+         <Joke
+            key={j.id}
+            text={j.text}
+            votes={j.votes}
+            upvote={() => this.handleVotes(j.id, 1)}
+            downvote={() => this.handleVotes(j.id, -1)}
+         />
       ));
 
       return (
          <div className="JokesList">
             <div className="JokesList-sidebar">
                <h1 className="JokesList-title"><span>Dad</span> Jokes</h1>
-               <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+               <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' alt="sidebar emoji" />
                <button className="JokesList-getmore">New Jokes</button>
             </div>
             <div className="JokesList-jokes">
