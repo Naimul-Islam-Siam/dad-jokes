127142474ea67985cb85d64bd042374bf70024e0
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-12-24 01:16:23 +0600

    Updates

---
+++ precache-manifest.31b52594de7b8caf7037207066f1b5ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdae4f1001586ff4c2eb55643f4732e",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "118fd7c7cafa4dec2de6",
+    "url": "/dad-jokes/static/css/main.5bc34586.chunk.css"
+  },
+  {
+    "revision": "cafdc3056543d8f17ccd",
+    "url": "/dad-jokes/static/js/2.4e71cc99.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/dad-jokes/static/js/2.4e71cc99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "118fd7c7cafa4dec2de6",
+    "url": "/dad-jokes/static/js/main.6becf69f.chunk.js"
+  },
+  {
+    "revision": "fd60d7bd1779161ad6dc",
+    "url": "/dad-jokes/static/js/runtime-main.2250a598.js"
+  }
+]);
