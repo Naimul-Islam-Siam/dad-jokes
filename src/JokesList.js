f50bc447c67e5db683e480344900cba816a1edf5
Naimul-Islam-Siam <hallowjokes4@gmail.com>
2019-12-24 01:01:09 +0600

    avoid duplicate jokes

--- src/JokesList.js
+++ src/JokesList.js
@@ -15,25 +15,38 @@ export default class JokesList extends Component {
          //if local storage has jokes, fetch them, otherwise make jokes and empty array []
          jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
       };
+
+      this.seenJokes = new Set(this.state.jokes.map(j => j.text));
+
       this.handleClick = this.handleClick.bind(this);
    }
 
    componentDidMount() {
       if (this.state.jokes.length === 0) {
+         this.setState({ loading: true });
          this.getJokes();
       }
    }
 
    async getJokes() {
-      let jokes = [];
-      while (jokes.length < this.props.numJokesToGet) {
-         let response = await axios.get("https://icanhazdadjoke.com/", { headers: { Accept: "application/json" } });
-         jokes.push({ id: uuid(), text: response.data.joke, votes: 0 });
+      try {
+         let jokes = [];
+         while (jokes.length < this.props.numJokesToGet) {
+            let response = await axios.get("https://icanhazdadjoke.com/", { headers: { Accept: "application/json" } });
+            let newJoke = response.data.joke;
+
+            if (!this.seenJokes.has(newJoke)) {
+               jokes.push({ id: uuid(), text: newJoke, votes: 0 });
+            }
+         }
+         this.setState(st => ({
+            loading: false, jokes: [...st.jokes, ...jokes]
+         }), () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+         );
+      } catch (err) {
+         alert(err);
+         this.setState({ loading: false });
       }
-      this.setState(st => ({
-         loading: false, jokes: [...st.jokes, ...jokes]
-      }), () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
-      );
    }
 
    handleClick() {
